5a46501863bdcd86ee63ce3db238ad8711a5c960
Edu <eduin3221@gmail.com>
2025-03-03 21:29:12 -0600

    Trabajos en semana 8

---
+++ Tienda2025/src/main/java/com/tienda/controller/PruebasController.java
@@ -0,0 +1,42 @@
+package com.tienda.controller;
+
+import com.tienda.domain.Categoria;
+import com.tienda.service.CategoriaService;
+import com.tienda.service.ProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/pruebas")
+public class PruebasController {
+
+    @Autowired
+    private ProductoService productoService;
+
+    @Autowired
+    private CategoriaService categoriaService;
+
+    @GetMapping("/listado")
+    public String listado(Model model) {
+        var productos = productoService.getProductos(false);
+        var categorias = categoriaService.getCategorias(false);
+        model.addAttribute("productos", productos);
+        model.addAttribute("totalProductos", productos.size());
+        model.addAttribute("categorias", categorias);
+        return "/pruebas/listado";
+    }
+
+    @GetMapping("/listado/{idCategoria}")
+    public String listado(@PathVariable Long idCategoria, Model model) {
+        Categoria categoria = new Categoria();
+        categoria.setIdCategoria(idCategoria);
+        var productos = categoriaService.getCategoria(categoria).getProductos();
+        var categorias = categoriaService.getCategorias(false);
+        model.addAttribute("productos", productos);
+        model.addAttribute("totalProductos", productos.size());
+        model.addAttribute("categorias", categorias);
+        return "/pruebas/listado";
+    }
+}
